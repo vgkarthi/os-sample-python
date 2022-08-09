42df829bc8e94ba4a45a4377517c620d61cbbd74
vgkarthi <vgkarthi@gmail.com>
2022-08-09 10:50:08 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello Karthi !!!"
+    return "Hello Karthikeyan VG !!!"
 
 if __name__ == "__main__":
     application.run()
